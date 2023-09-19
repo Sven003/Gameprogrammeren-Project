0e79a41efdaad2a95c042bccfaae322f32dc5dd8
DerpyBurgerz <144908293+DerpyBurgerz@users.noreply.github.com>
2023-09-19 15:15:06 +0200

    2 dezelfde paddles zijn nu zichtbaar met behulp van een paddle class
    Ik heb een Class aangemaakt waarmee ik in deze versie 2 paddles op het scherm kan laten zien. Ik heb de coords van sprite test gebruikt.
    Ze hebben op dit moment nog allebei dezelfde kleur, maar dat moet makkelijk te veranderen zijn.
    Ik heb de variabelen positionPaddle1 en positionPaddle2 aangemaakt

--- Pong Project/Pong Project/Game1.cs
+++ Pong Project/Pong Project/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Pong_Project.Objects;
+//using SharpDX.Direct2D1;
 
 namespace Pong_Project
 {
@@ -8,6 +10,8 @@ namespace Pong_Project
 	{
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
+		Paddle paddle1, paddle2;
+		Vector2 positionPaddle1, positionPaddle2;
 
 		public Game1()
 		{
@@ -25,28 +29,42 @@ namespace Pong_Project
 
 		protected override void LoadContent()
 		{
-			_spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+			paddle1 = new Paddle(Content);
+            paddle2 = new Paddle(Content);
 
-			// TODO: use this.Content to load your game content here
-		}
+            // TODO: use this.Content to load your game content here
+        }
 
 		protected override void Update(GameTime gameTime)
 		{
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 
-			// TODO: Add your update logic here
+			paddle1.Update(gameTime);
+            paddle2.Update(gameTime);
+			positionPaddle1.X = (789);
+			positionPaddle1.Y = (210);
+			positionPaddle2.X = (0);
+			positionPaddle2.Y = (210);
+			
+			//Ik heb eerst dit stukje hieronder getest, maar dat werkte niet. De oplossing hierboven wel en ik heb geen idee waarom.
+			//positionPaddle1 = new Vector2(789, 210);
+            //positionPaddle1 = new Vector2(0, 210);
 
-			base.Update(gameTime);
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
 		}
 
 		protected override void Draw(GameTime gameTime)
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
+			paddle1.draw(_spriteBatch, positionPaddle1);
+            paddle2.draw(_spriteBatch, positionPaddle2);
+            // TODO: Add your drawing code here
 
-			// TODO: Add your drawing code here
-
-			base.Draw(gameTime);
+            base.Draw(gameTime);
 		}
 	}
 }
--- Pong Project/Pong Project/Objects/Paddle.cs
+++ Pong Project/Pong Project/Objects/Paddle.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 
 namespace Pong_Project.Objects
 {
-	internal class Paddle
+	
+	public class Paddle
 	{
-	}
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Vector2 paddlePosition;
+		Texture2D paddle;
+
+        public Paddle(ContentManager Content)
+        {
+            paddle = Content.Load<Texture2D>("paddleBlue");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        public void draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(paddle, position, Color.White);
+            spriteBatch.End();  
+        }
+    }
 }
