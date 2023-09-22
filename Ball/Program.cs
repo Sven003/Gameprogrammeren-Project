1b2d7f672399409e341860b5616156b6402c3655
Sven003 <50364272+Sven003@users.noreply.github.com>
2023-09-22 11:29:49 +0200

    bewegen ball tegen y waardes

---
+++ Ball/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Ball.ballGame();
+game.Run();
---
+++ Ball/ballGame.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Ball
+{
+    public class ballGame : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        Texture2D ball;
+        Vector2 ballPosition;
+        Vector2 movement;
+        public ballGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+           
+        }
+
+        static void main()
+        {
+            ballGame game = new ballGame();
+            game.Run();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            ball = Content.Load<Texture2D>("avgBallSmall");
+            ballPosition = new Vector2(390, 240);//draw the initial position of the ball in the center of the screen.
+            movement = new Vector2(0, 2);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            
+            ballPosition = ballPosition + movement;//updates the ballposition with the movement to create the velocity of the ball
+
+            if (ballPosition.Y <= 0 || ballPosition.Y >= 466)//if ball reaches the borders, reverse the movement
+            {
+                movement = -movement;
+            }
+            
+            
+           
+            
+            
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(ball, ballPosition, Color.White);
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
